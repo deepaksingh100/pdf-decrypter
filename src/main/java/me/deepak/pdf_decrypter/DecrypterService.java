3d7440e611506e2fbb01e513cfa5f8a75a6e24b4
Deepak Singh <deepak.singh3@go-mmt.com>
2021-07-22 17:18:58 +0530

    refactored

--- src/main/java/me/deepak/pdf_decrypter/DecrypterService.java
+++ src/main/java/me/deepak/pdf_decrypter/DecrypterService.java
@@ -7,25 +7,31 @@ import org.apache.commons.io.FileUtils;
 import org.apache.pdfbox.pdmodel.PDDocument;
 
 public class DecrypterService {
-	
+
 	private DecrypterService() {
-		
+
 	}
-	
+
 	public static void decrypt(File file, String password) throws IOException {
+		File fileToBeDecrypted = getFileToBeDecrypted(file);
+		PDDocument pdfDocument = PDDocument.load(fileToBeDecrypted, password);
+		pdfDocument.setAllSecurityToBeRemoved(true);
+		pdfDocument.save(fileToBeDecrypted);
+		pdfDocument.close();
+	}
+
+	private static File getFileToBeDecrypted(File file) throws IOException {
 		String fileName = file.getName();
 		File parentFile = file.getParentFile();
 		String parentPath = File.separator;
 		if (parentFile != null) {
 			parentPath = parentFile.getPath().concat(parentPath);
 		}
-		String decryptedFileName = parentPath.concat(fileName.substring(0, fileName.length() - ".pdf".length()).concat("-decrypted.pdf"));
+		String decryptedFileName = parentPath
+				.concat(fileName.substring(0, fileName.length() - ".pdf".length()).concat("-decrypted.pdf"));
 		File decyptedFile = new File(decryptedFileName);
 		FileUtils.copyFile(file, decyptedFile);
-		PDDocument pdfDocument = PDDocument.load(decyptedFile, password);
-		pdfDocument.setAllSecurityToBeRemoved(true);
-		pdfDocument.save(decyptedFile);
-		pdfDocument.close();
+		return decyptedFile;
 	}
 
 }
